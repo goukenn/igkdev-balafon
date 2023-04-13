ed332ddac0a48638b9ff1ee301e07ffb9a805b0f
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2023-04-13 13:26:23 +0200

    update redirection

--- src/Lib/igk/igk_redirection.php
+++ src/Lib/igk/igk_redirection.php
@@ -4,5 +4,22 @@
 // @date: 20230413 12:42:49
 // @desc: handle redirection 
 
-
+if (defined("IGK_REDIRECTION") && (IGK_REDIRECTION == 1))
+    return;
+define('IGK_REDIRECTION', 1);
+if (!isset($redirect)) {
+    require_once __DIR__ . '/igk_framework.php';
+    $server_info = (object)array();
+    foreach (array(
+        "REQUEST_URI" => '',
+        "SERVER_PROTOCOL" => '',
+        "REDIRECT_STATUS" => '',
+        "REDIRECT_URL" => '',
+        "REDIRECT_REQUEST_METHOD" => 'GET',
+        "REDIRECT_QUERY_STRING" => ''
+    ) as $k => $v) {
+        $server_info->$k = igk_getv($_SERVER, $k, $v);
+    }
+    $redirect = $server_info->{'REQUEST_URI'};
+}
 igk_io_handle_system_command($redirect);
