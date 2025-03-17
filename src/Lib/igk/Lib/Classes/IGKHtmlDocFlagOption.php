0b4b490bb0e57626f09dfa6423486df17dd1b7d4
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2025-03-17 23:31:41 +0100

    add flag option

---
+++ src/Lib/igk/Lib/Classes/IGKHtmlDocFlagOption.php
@@ -0,0 +1,35 @@
+<?php
+// @author: C.A.D. BONDJE DOUE
+// @file: IGKHtmlDocFlagOption.php
+// @date: 20250316 18:26:53
+namespace IGK;
+
+
+///<summary></summary>
+/**
+* 
+* @package IGK
+* @author C.A.D. BONDJE DOUE
+*/
+class IGKHtmlDocFlagOption{
+    /**
+     * disable powered by 
+     * @var mixed
+     */
+    var $noPowered;
+    /**
+     * disable use of balafon js
+     * @var mixed
+     */
+    var $noCoreCss;
+    /**
+     * disable core js 
+     * @var mixed
+     */
+    var $noCoreJs;
+    /**
+     * disable cookie warning
+     * @var mixed
+     */
+    var $noCookieWarn;
+}
