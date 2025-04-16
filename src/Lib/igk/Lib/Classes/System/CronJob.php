4ca71c3b45d6626cb7664a8528444fce49347d3d
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2025-04-16 13:32:20 +0200

    commit 20250416-13:32:20

--- src/Lib/igk/Lib/Classes/System/CronJob.php
+++ src/Lib/igk/Lib/Classes/System/CronJob.php
@@ -115,6 +115,8 @@ class CronJob
      */
     public static function ExecuteCronList(array $crons, callable $handle_cron_script, ?string $exclude_fs = null)
     {
+        class_alias(\IGK\System\Cron\CommandHelper::class, 'CommandHelper');
+        
         $dir = IGK_LIB_DIR.'/Crons';
         $exclude_fs = $exclude_fs ?? IGK_LIB_DIR.'/cron-tab.php';
         $json_db_flag = JSON_UNESCAPED_SLASHES;
