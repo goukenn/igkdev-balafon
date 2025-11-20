708be9498d5b3fce452f8752a6f75ba04d6dfb63
C.A.D. Bondje Doue <bondje.doue@gmail.com>
2025-11-20 18:02:20 +0100

    commit 20251120-18:02:20

--- src/Lib/igk/Lib/Classes/Composer/Installer.php
+++ src/Lib/igk/Lib/Classes/Composer/Installer.php
@@ -4,19 +4,41 @@
 // @date: 20251120 16:01:35
 namespace IGK\Composer;
 
+defined('IGK_VERSION') && die('already defined');
 
+require_once __DIR__.'/../../../igk_framework.php';
 /**
 * 
 * @package IGK\Composer
 * @author C.A.D. BONDJE DOUE
 */
 class Installer{
-    public static function PostInstall(){
-        echo 'running post install', PHP_EOL;
-        echo 'the current cwd: '.getcwd() , PHP_EOL;
+    /**
+     * 
+     * @return never 
+     */
+    public static function PostInstall(){        
+         echo 'running post install', PHP_EOL;
+        $chdir = getcwd();
+        $argv = igk_getv($_SERVER, 'argv');
+        igk_wln('VERSION: '.IGK_VERSION);
+        igk_wln('cwd: '.$chdir); 
+        $vendor_dir = $chdir.'/vendor';
+        $args = [];
+        if (is_dir($vendor_dir)){
+            $args['--vendor-dir'] =$vendor_dir;
+        }
+
+        igk_wln_e($argv);
+
+        $cli = IGK_LIB_DIR.'/bin/balafon';
+        $wdir = IGK_LIB_DIR;
+        `chown -R www-data:www-data {$wdir}`;
+        // + | init project 
+        `cd {$chdir} && $cli --init --noconfig --reset ./`;
     }
     public static function PostUpdate(){
         echo 'running post update', PHP_EOL;
-        echo 'the current cwd: '.getcwd() , PHP_EOL;
+        
     }
 }
